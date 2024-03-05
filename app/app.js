15d49672bd68d44724a429fcefd85999ce1cf1ba
enribarrola <enribarrola@hotmail.com>
2024-03-05 21:36:45 +0000

    funca la vpn

--- app/app.js
+++ app/app.js
@@ -33,7 +33,7 @@ app.use((err, req, res, next) => {
 });
 
 // Iniciar el servidor
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`La aplicación está escuchando en http://localhost:${port}`);
 });
